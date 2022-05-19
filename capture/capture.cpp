c837da673c71255f40ba86807e7e58fe27b834d3
r.ito <homing0321r4cfw@yahoo.co.jp>
2022-05-19 13:23:28 +0900

    opencvでキャプチャできる所まで

---
+++ capture/capture.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <Windows.h>
+#include <tchar.h>		// TCHAR型
+using cv::Mat;
+
+BITMAPINFOHEADER createBitmapHeader(int width, int height)
+{
+    BITMAPINFOHEADER  bi;
+
+    // create a bitmap
+    bi.biSize = sizeof(BITMAPINFOHEADER);
+    bi.biWidth = width;
+    bi.biHeight = -height;  //this is the line that makes it draw upside down or not
+    bi.biPlanes = 1;
+    bi.biBitCount = 32;
+    bi.biCompression = BI_RGB;
+    bi.biSizeImage = 0;
+    bi.biXPelsPerMeter = 0;
+    bi.biYPelsPerMeter = 0;
+    bi.biClrUsed = 0;
+    bi.biClrImportant = 0;
+
+    return bi;
+}
+
+Mat captureScreenMat(HWND hwnd)
+{
+    Mat src;
+
+    // get handles to a device context (DC)
+    HDC hwindowDC = GetDC(hwnd);
+    HDC hwindowCompatibleDC = CreateCompatibleDC(hwindowDC);
+    SetStretchBltMode(hwindowCompatibleDC, COLORONCOLOR);
+
+    // define scale, height and width
+    int screenx = GetSystemMetrics(SM_XVIRTUALSCREEN);
+    int screeny = GetSystemMetrics(SM_YVIRTUALSCREEN);
+    int width = GetSystemMetrics(SM_CXVIRTUALSCREEN);
+    int height = GetSystemMetrics(SM_CYVIRTUALSCREEN);
+
+    // create mat object
+    src.create(height, width, CV_8UC4);
+
+    // create a bitmap
+    HBITMAP hbwindow = CreateCompatibleBitmap(hwindowDC, width, height);
+    BITMAPINFOHEADER bi = createBitmapHeader(width, height);
+
+    // use the previously created device context with the bitmap
+    SelectObject(hwindowCompatibleDC, hbwindow);
+
+    // copy from the window device context to the bitmap device context
+    StretchBlt(hwindowCompatibleDC, 0, 0, width, height, hwindowDC, screenx, screeny, width, height, SRCCOPY);  //change SRCCOPY to NOTSRCCOPY for wacky colors !
+    GetDIBits(hwindowCompatibleDC, hbwindow, 0, height, src.data, (BITMAPINFO*)&bi, DIB_RGB_COLORS);            //copy from hwindowCompatibleDC to hbwindow
+
+    // avoid memory leak
+    DeleteObject(hbwindow);
+    DeleteDC(hwindowCompatibleDC);
+    ReleaseDC(hwnd, hwindowDC);
+
+    return src;
+}
+
+template<class T> void print(const T& value) {
+	std::cout << value << std::endl;
+}
+
+int main() {
+	print("a"); // 出力結果: "a\n"
+	print(999); // 出力結果: "999\n"
+
+    // capture image
+    HWND hwnd = ::FindWindow(nullptr, _T("Puyo Puyo Champions"));
+
+    Mat src = captureScreenMat(hwnd);
+
+    // save img
+    cv::imwrite("Screenshot.png", src);
+
+    return 0;
+}
+
